f16b51d31ec3e6a65f489141c3d53af9e104fab1
hsiegersma <hylkesiegersma@gmail.com>
2025-06-04 09:48:12 +0200

    Update Globals.h
    gug

--- MarvelDMA/Globals.h
+++ MarvelDMA/Globals.h
@@ -25,30 +25,32 @@ inline namespace MarvelDMA {
 
 		// Offsets removed - please do not update this just to use it to cheat
 		struct Offsets {
-			uintptr_t uworld = 0x0;
-			uintptr_t gameInstance = 0x0;
-			uintptr_t playersArray = 0x0;
-			uintptr_t playerController = 0x0;
-			uintptr_t playerCameraManager = 0x0;
-			uintptr_t playerCameraCache = 0x0;
-			uintptr_t playerCameraPOV = 0x0;
-			uintptr_t localPawn = 0x0;
-			uintptr_t gameState = 0x0;
-			uintptr_t playerArray = 0x0;
-			uintptr_t pawnPrivate = 0x0;
-			uintptr_t childActorComponent = 0x0;
-			uintptr_t childActor = 0x0;
-			uintptr_t mesh = 0x0;
-			uintptr_t marvelCharacter = 0x0;
-			uintptr_t defaultFov = 0x0;
-			uintptr_t teamState = 0x0;
-			uintptr_t heroID = 0x0;
+			uintptr_t gworld = 0xE5E7000;
+			uintptr_t uworld; // this will hold mem.Read<uintptr_t>(gworld) later
 
-			uintptr_t lastSubmitTime = 0x0;
-			uintptr_t lastRenderTime = 0x0;
+			uintptr_t gameInstance = 0x270;
+			uintptr_t playersArray = 0x40;
+			uintptr_t playerController = 0x38;
+			uintptr_t playerCameraManager = 0x6c8;
+			uintptr_t playerCameraCache = 0x1bb0;
+			uintptr_t playerCameraPOV = 0x10;
+			uintptr_t localPawn = 0x6b8;
+			uintptr_t gameState = 0x210;
+			uintptr_t playerArray = 0x610;
+			uintptr_t pawnPrivate = 0x648;
+			uintptr_t childActorComponent = 0x450;
+			uintptr_t childActor = 0x478;
+			uintptr_t mesh = 0x5f8;
+			uintptr_t marvelCharacter = 0x950;
+			uintptr_t defaultFov = 0x614;
+			uintptr_t teamState = 0x630;
+			uintptr_t heroID = 0xa60;
 
-			uintptr_t componentToWorld = 0x0;
-			uintptr_t boneArray = 0x0;
+			uintptr_t lastSubmitTime = 0x380;
+			uintptr_t lastRenderTime = 0x384;
+
+			uintptr_t componentToWorld = 0x1c0;
+			uintptr_t boneArray = 0xec0;
 		} offsets;
 
 		struct Settings {
